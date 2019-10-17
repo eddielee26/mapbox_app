a93932cd1c08611d57229938639aa6ce49b55a36
eddie <eddielee001@gmail.com>
2019-10-17 12:46:32 -0500

    adds map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div id='map'></div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+<style>
+  body { 
+    margin:0; 
+    padding:0; 
   }
-}
+  #map { 
+    top:0; 
+    bottom:0; 
+    height:500px; 
+  }
+</style>
+
+<script>
+/* global mapboxgl */
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {},
+  mounted: function() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZWRkaWVsZWU5MyIsImEiOiJjazF1ejR0NzExNzFpM2xwNGhlbmQ2b2p0In0.TWYYKg_6Lqkj4e9cDNd9XA';
+    var map = new mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+      center: [-74.50, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+  },
+  methods: {}
+};
 </script>
